dfdc6393e1a0edd2a6625d998bcb88e3625f45a1
Dean Hillan <dean.hillan@gmail.com>
2014-03-31 15:01:12 +1100

    Initial commit

---
+++ first_zero_in_binary_number.py
@@ -0,0 +1,11 @@
+"""
+Algorithm to find the index of first zero in a binary number where index zero is furtherest right in array
+"""
+number = 8
+bin_number = bin(number)
+print "Number is %d" %number
+
+def get_first_zero(number):
+
+
+
