4fda9a8d1879bd3777fa0280b98cac7fd7fed3b3
Dean Hillan <dean.hillan@gmail.com>
2014-03-31 15:19:17 +1100

    Finished

--- first_zero_in_binary_number.py
+++ first_zero_in_binary_number.py
@@ -1,11 +1,17 @@
 """
 Algorithm to find the index of first zero in a binary number where index zero is furtherest right in array
 """
-number = 8
+number = 20
 bin_number = bin(number)
 print "Number is %d" %number
+print "Binary represendation is %s" %bin_number
 
-def get_first_zero(number):
+def get_first_zero(bin_number):
+	for i in reversed(range(len(bin_number))):
+		if bin_number[i] == "1":
+			return len(bin_number) - i
+	return -1
+print "Index of the first 1 is: %d" %get_first_zero(bin_number)
 
 
 
